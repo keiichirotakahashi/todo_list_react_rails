1e157021be809851d4c9546253ce2dcf8fc5e082
Keiichiro Takahashi <k16takahashi125@gmail.com>
2020-07-03 12:49:47 +0900

    Refactor components

--- app/javascript/components/Main.js
+++ app/javascript/components/Main.js
@@ -1,41 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Flash from './Flash';
-import TaskForm from './TaskForm';
 import Task from './Task';
 
 const Main = () => {
-  const [tasks, setTasks] = useState([]);
-  const [taskForm, setTaskForm] = useState({
-    name: '',
-    due_on: ''
-  });
-  const [taskFormErrors, setTaskFormErrors] = useState([]);
-  const [modalTaskForm, setModalTaskForm] = useState({
-    name: '',
-    due_on: ''
-  });
-  const [modalTaskFormErrors, setModalTaskFormErrors] = useState([]);
   const [flash, setFlash] = useState({
     isVisible: false,
     status: 'hidden',
     message: ''
-  });  
-  const csrf = document.querySelector("meta[name='csrf-token']").getAttribute("content");
+  });
   let timer = null;
-
-  useEffect(() => {
-    const getTasks = async () => {
-      try {
-        const response = await fetch('/api/v1/tasks');
-        const json = await response.json();
-        setTasks(json);
-      } catch (error) {
-        showErrorFlash();
-      }
-    }
-
-    getTasks();
-  }, []);
 
   const showNoticeFlash = (message) => {
     setFlash({
@@ -69,166 +42,17 @@ const Main = () => {
   }
 
   const removeFlashLater = () => {
-    timer = setTimeout(() => {removeFlash()}, 3000);
+    timer = setTimeout(() => { removeFlash() }, 3000);
   }
 
-  const handleTaskFormChange = (event) => {
-    const { name, value } = event.target;
-    setTaskForm({...taskForm, [name]: value});
-  }
-
-  const handleTaskFormSubmit = async (event) => {
-    event.preventDefault();
-    setTaskFormErrors([]);
-    removeFlashNow();
-
-    try {
-      const response = await fetch('/api/v1/tasks', {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json; charset=utf-8",
-          "X-CSRF-Token": csrf
-        },
-        body: JSON.stringify({task: taskForm})
-      });
-
-      if (response.ok) {
-        const json = await response.json();
-        tasks.unshift(json);
-        setTasks(tasks);
-        setTaskForm({
-          name: '',
-          due_on: ''
-        });
-        showNoticeFlash('ToDoを作成しました。');
-        return;
-      }
-
-      const json = await response.json();
-      setTaskFormErrors(json);
-      showErrorFlash('ToDoの作成に失敗しました。');
-    } catch(error) {
-      showErrorFlash();
-    }   
-  }
-
-  const buildModalTaskForm = async (id) => {
-    removeFlashNow();
-
-    try {
-      const response = await fetch(`/api/v1/tasks/${id}`);
-      const json = await response.json();
-      setModalTaskForm({
-        name: json.name,
-        due_on: json.due_on
-      });
-    } catch(error) {
-      showErrorFlash();
-    }
-  }
-
-  const handleModalTaskFormChange = (event) => {
-    const { name, value } = event.target;
-    setModalTaskForm({...modalTaskForm, [name]: value});
-  }
-
-  const handleModalTaskFormSubmit = (event, id) => {
-    event.preventDefault();
-    patchTask(id, modalTaskForm);
-  }
-
-  const toggleStatus = (id, status) => {
-    const toggledStatus = status === 'todo' ? 'done' : 'todo';
-    patchTask(id, {status: toggledStatus});
-  }
-
-  const removeModalTaskFormErrors = () => {
-    setModalTaskFormErrors([]);
-  }
-
-  const patchTask = async (id, attributes) => {
-    removeModalTaskFormErrors();
-    removeFlashNow();
-
-    try {
-      const response = await fetch(`/api/v1/tasks/${id}`, {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json; charset=utf-8",
-          "X-CSRF-Token": csrf
-        },
-        body: JSON.stringify({task: attributes})
-      });
-
-      if (response.ok) {
-        const json = await response.json();
-        const copiedTasks = tasks.map(task => {
-          if (task.id === json.id) return json;
-          return task;
-        });
-        setTasks(copiedTasks);
-        showNoticeFlash('ToDoを更新しました。');
-        return;
-      }
-
-      const json = await response.json();
-      setModalTaskFormErrors(json);
-      showErrorFlash('ToDoの更新に失敗しました。');
-    } catch(error) {
-      showErrorFlash();
-    }
-  }
-
-  const resetModalTaskForm = () => {
-    setModalTaskForm({
-      name: '',
-      due_on: ''
-    });
-  }
-
-  const removeTask = async (id) => {
-    removeFlashNow();
-
-    try {
-      const response = await fetch(`/api/v1/tasks/${id}`, {
-        method: "DELETE",
-        headers: {"X-CSRF-Token": csrf}
-      });
-      const json = await response.json();
-      setTasks(tasks.filter(task => {
-        return task.id !== json.id;
-      }));
-      showNoticeFlash('ToDoを削除しました。');
-    } catch(error) {
-      showErrorFlash();
-    }
-  }
-
-  const taskComponents = tasks.map((task) =>
-    <Task
-      key={task.id}
-      taskData={task}
-      toggleStatus={toggleStatus}
-      removeTask={removeTask}
-      modalTaskFormData={modalTaskForm}
-      formErrorsData={modalTaskFormErrors}
-      buildModalTaskForm={buildModalTaskForm}
-      handleModalTaskFormChange={handleModalTaskFormChange}
-      handleModalTaskFormSubmit={handleModalTaskFormSubmit}
-      removeModalTaskFormErrors={removeModalTaskFormErrors}
-      resetModalTaskForm={resetModalTaskForm} />
-  );
-
-  return(
+  return (
     <main className='main'>
       <Flash flashData={flash} />
       <div className='main-box'>
-        <TaskForm
-          taskFormData={taskForm}
-          formErrorsData={taskFormErrors}
-          handleTaskFormChange={handleTaskFormChange}
-          handleTaskFormSubmit={handleTaskFormSubmit} />
-        {taskComponents}
+        <Task
+          showNoticeFlash={showNoticeFlash}
+          showErrorFlash={showErrorFlash}
+          removeFlashNow={removeFlashNow} />
       </div>
     </main>
   );
--- app/javascript/components/Task.js
+++ app/javascript/components/Task.js
@@ -1,88 +1,191 @@
-import React, { useState } from 'react';
-import TaskFormModal from './TaskFormModal';
+import React, { useState, useEffect } from 'react';
+import TaskForm from './TaskForm';
+import TaskCard from './TaskCard';
 
 const Task = (props) => {
-  const [isTaskFormModalOpen, setIsTaskFormModalOpen] = useState(false);
-  const id = props.taskData.id;
-  const name = props.taskData.name;
-  const dueOn = new Date(props.taskData.due_on.replace(/-/g, '/'));
-  const dueDate = `${dueOn.getFullYear()}年${dueOn.getMonth() + 1}月${dueOn.getDate()}日`;
-  const createdAt = new Date(props.taskData.created_at);
-  const createdDate = `${createdAt.getFullYear()}年${createdAt.getMonth() + 1}月${createdAt.getDate()}日`;
-  const status = props.taskData.status;
+  const [tasks, setTasks] = useState([]);
+  const [taskForm, setTaskForm] = useState({
+    name: '',
+    due_on: ''
+  });
+  const [taskFormErrors, setTaskFormErrors] = useState([]);
+  const [modalTaskForm, setModalTaskForm] = useState({
+    name: '',
+    due_on: ''
+  });
+  const [modalTaskFormErrors, setModalTaskFormErrors] = useState([]);
+  const csrf = document.querySelector("meta[name='csrf-token']").getAttribute("content");
 
-  const translateStatus = (status) => {
-    switch (status) {
-      case 'todo':
-        return '未完了';
-      case 'done':
-        return '完了';
+  useEffect(() => {
+    const getTasks = async () => {
+      try {
+        const response = await fetch('/api/v1/tasks');
+        const json = await response.json();
+        setTasks(json);
+      } catch (error) {
+        props.showErrorFlash();
+      }
+    }
+
+    getTasks();
+  }, []);
+
+  const handleTaskFormChange = (event) => {
+    const { name, value } = event.target;
+    setTaskForm({...taskForm, [name]: value});
+  }
+
+  const handleTaskFormSubmit = async (event) => {
+    event.preventDefault();
+    setTaskFormErrors([]);
+    props.removeFlashNow();
+
+    try {
+      const response = await fetch('/api/v1/tasks', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          "X-CSRF-Token": csrf
+        },
+        body: JSON.stringify({task: taskForm})
+      });
+
+      if (response.ok) {
+        const json = await response.json();
+        tasks.unshift(json);
+        setTasks(tasks);
+        setTaskForm({
+          name: '',
+          due_on: ''
+        });
+        props.showNoticeFlash('ToDoを作成しました。');
+        return;
+      }
+
+      const json = await response.json();
+      setTaskFormErrors(json);
+      props.showErrorFlash('ToDoの作成に失敗しました。');
+    } catch(error) {
+      props.showErrorFlash();
+    }   
+  }
+
+  const buildModalTaskForm = async (id) => {
+    props.removeFlashNow();
+
+    try {
+      const response = await fetch(`/api/v1/tasks/${id}`);
+      const json = await response.json();
+      setModalTaskForm({
+        name: json.name,
+        due_on: json.due_on
+      });
+    } catch(error) {
+      props.showErrorFlash();
     }
   }
 
-  const handleClickEdit = (id) => {
-    props.buildModalTaskForm(id);
-    setIsTaskFormModalOpen(true);
+  const handleModalTaskFormChange = (event) => {
+    const { name, value } = event.target;
+    setModalTaskForm({...modalTaskForm, [name]: value});
   }
 
-  const handleClickModalClose = () => {
-    setIsTaskFormModalOpen(false);
-    props.resetModalTaskForm();
-    props.removeModalTaskFormErrors();
+  const handleModalTaskFormSubmit = (event, id) => {
+    event.preventDefault();
+    patchTask(id, modalTaskForm);
   }
+
+  const toggleStatus = (id, status) => {
+    const toggledStatus = status === 'todo' ? 'done' : 'todo';
+    patchTask(id, {status: toggledStatus});
+  }
+
+  const removeModalTaskFormErrors = () => {
+    setModalTaskFormErrors([]);
+  }
+
+  const patchTask = async (id, attributes) => {
+    removeModalTaskFormErrors();
+    props.removeFlashNow();
+
+    try {
+      const response = await fetch(`/api/v1/tasks/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          "X-CSRF-Token": csrf
+        },
+        body: JSON.stringify({task: attributes})
+      });
+
+      if (response.ok) {
+        const json = await response.json();
+        const copiedTasks = tasks.map(task => {
+          if (task.id === json.id) return json;
+          return task;
+        });
+        setTasks(copiedTasks);
+        props.showNoticeFlash('ToDoを更新しました。');
+        return;
+      }
+
+      const json = await response.json();
+      setModalTaskFormErrors(json);
+      props.showErrorFlash('ToDoの更新に失敗しました。');
+    } catch(error) {
+      props.showErrorFlash();
+    }
+  }
+
+  const resetModalTaskForm = () => {
+    setModalTaskForm({
+      name: '',
+      due_on: ''
+    });
+  }
+
+  const removeTask = async (id) => {
+    props.removeFlashNow();
+
+    try {
+      const response = await fetch(`/api/v1/tasks/${id}`, {
+        method: "DELETE",
+        headers: {"X-CSRF-Token": csrf}
+      });
+      const json = await response.json();
+      setTasks(tasks.filter(task => {
+        return task.id !== json.id;
+      }));
+      props.showNoticeFlash('ToDoを削除しました。');
+    } catch(error) {
+      props.showErrorFlash();
+    }
+  }
+
+  const taskComponents = tasks.map((task) =>
+    <TaskCard
+      key={task.id}
+      taskData={task}
+      toggleStatus={toggleStatus}
+      removeTask={removeTask}
+      modalTaskFormData={modalTaskForm}
+      formErrorsData={modalTaskFormErrors}
+      buildModalTaskForm={buildModalTaskForm}
+      handleModalTaskFormChange={handleModalTaskFormChange}
+      handleModalTaskFormSubmit={handleModalTaskFormSubmit}
+      removeModalTaskFormErrors={removeModalTaskFormErrors}
+      resetModalTaskForm={resetModalTaskForm} />
+  );
 
   return(
-    <div className='task'>
-      <h2 className='task__name'>
-        {name}
-      </h2>
-      <div className='task-body'>
-        <div className='task-body-info'>
-          <div className='task-body-info-due-date'>
-            <div className='task-body-info-due-date__label'>
-              期限：
-            </div>
-            <div className='task-body-info-due-date__value'>
-              {dueDate}
-            </div>
-          </div>
-          <div className='task-body-info-created-date'>
-            <div className='task-body-info-created-date__label'>
-              作成日：
-            </div>
-            <div className='task-body-info-created-date__value'>
-              {createdDate}
-            </div>
-          </div>
-        </div>
-        <div className='task-body-buttons'>
-          <div className='task-body-buttons-top'>
-            <button className={`task-body-buttons-top__status--${status}`}
-              onClick={() => {props.toggleStatus(id, status)}}>
-              {translateStatus(props.taskData.status)}
-            </button>
-          </div>
-          <div className='task-body-buttons-bottom'>
-            <button className='task-body-buttons-bottom__edit'
-              onClick={() => {handleClickEdit(id)}}>
-              編集
-            </button>
-            <button className='task-body-buttons-bottom__delete'
-              onClick={() => {props.removeTask(id)}}>
-              削除
-            </button>
-          </div>
-        </div>
-      </div>
-      <TaskFormModal
-        id={id}
-        isTaskFormModalOpen={isTaskFormModalOpen}
-        handleClickModalClose={handleClickModalClose}
-        modalTaskFormData={props.modalTaskFormData}
-        formErrorsData={props.formErrorsData}
-        handleModalTaskFormChange={props.handleModalTaskFormChange}
-        handleModalTaskFormSubmit={props.handleModalTaskFormSubmit} />
-    </div>
+    <React.Fragment>
+      <TaskForm
+        taskFormData={taskForm}
+        formErrorsData={taskFormErrors}
+        handleTaskFormChange={handleTaskFormChange}
+        handleTaskFormSubmit={handleTaskFormSubmit} />
+      {taskComponents}
+    </React.Fragment>
   );
 }
 
