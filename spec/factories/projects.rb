ae2c2f2195adcffb778f05307920e2fd8e096196
Keiichiro Takahashi <k16takahashi125@gmail.com>
2020-07-04 11:14:56 +0900

    Add models/project_spec

---
+++ spec/factories/projects.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: projects
+#
+#  id         :bigint           not null, primary key
+#  name       :string           default(""), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_projects_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+FactoryBot.define do
+  factory :project do
+    association :user
+    name { 'プロジェクト' }
+  end
+end
