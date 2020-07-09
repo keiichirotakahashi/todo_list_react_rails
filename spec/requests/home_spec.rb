9365225247b94bc611709d8168d50f72d46890a0
Keiichiro Takahashi <k16takahashi125@gmail.com>
2020-07-09 09:34:10 +0900

    Modify requests/home_spec

---
+++ spec/requests/home_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Home', type: :request do
+  let(:user) { create(:user) }
+
+  describe 'GET /' do
+    subject do
+      get '/'
+      response
+    end
+
+    context 'as an authenticated user' do
+      before { sign_in user }
+
+      it { is_expected.to have_http_status 200 }
+    end
+
+    context 'as a guest' do
+      it { is_expected.to have_http_status 302 }
+      it { is_expected.to redirect_to '/login' }
+    end
+  end
+end
